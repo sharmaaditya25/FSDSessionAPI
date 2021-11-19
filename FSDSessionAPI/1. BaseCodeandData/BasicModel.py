ee886f58540f7b2e5e9acbe1d0f95b9aa12c823f
sharmaaditya25 <55826538+sharmaaditya25@users.noreply.github.com>
2021-11-19 11:45:39 +0530

    Add files via upload

---
+++ FSDSessionAPI/1. BaseCodeandData/BasicModel.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+# Load the csv file
+df = pd.read_csv(r"C:\Users\as14329\Desktop\FSDSessionAPI\1. BaseCodeandData\iris.csv")
+
+# Select independent and dependent variable
+X = df[["Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width"]]
+y = df["Class"]
+
+# Split the dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=50)
+
+# Feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test= sc.transform(X_test)
+
+# Instantiate the model
+classifier = RandomForestClassifier()
+
+# Fit the model
+classifier.fit(X_train, y_train)
+
+# Make pickle file of our model
+pickle.dump(classifier, open(r"C:\Users\as14329\Desktop\FSDSessionAPI\1. BaseCodeandData\model.pkl", "wb"))
+
+#Load the saved model
+model = pickle.load(open(r"C:\Users\as14329\Desktop\FSDSessionAPI\1. BaseCodeandData\model.pkl", "rb"))
+
+#Predictions
+output = model.predict(X_test)
